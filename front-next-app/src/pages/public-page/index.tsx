e81ca873c4dff4559fe68f609fabca07db7c46c5
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-17 22:49:44 +0300

    IN-5 fix: public-page - use PageWrapper and Layout

--- front-next-app/src/pages/public-page/index.tsx
+++ front-next-app/src/pages/public-page/index.tsx
@@ -1,9 +1,13 @@
-export default function UserProfile() {
-  return (
-    <>
-      <main>
-          UserProfile
-      </main>
-    </>
-  )
+import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
+import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+
+export function PublicPage() {
+    return (
+        <PageWrapper>
+            PublicPage
+        </PageWrapper>
+    )
 }
+
+PublicPage.getLayout = getLayout
+export default PublicPage
