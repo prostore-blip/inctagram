13f1be4f660f11038a9d2c0c8db7bdb54b315107
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-18 00:13:17 +0300

    IN-5 fix: refactor Layout for show/unshow Nav

--- front-next-app/components/Layout/Layout.tsx
+++ front-next-app/components/Layout/Layout.tsx
@@ -1,12 +1,23 @@
 import {PropsWithChildren} from "react";
 import {NextPage} from "next";
 import {Header} from "../Header/Header";
+import {Nav} from "../Nav/Nav";
+import s from "./Layout.module.scss"
+import {Main} from "../Main/Main";
+import {log} from "node:util";
 
-export const Layout: NextPage<PropsWithChildren> = ({children}) => {
+type Props = {
+    showNav?: boolean
+}
+export const Layout: NextPage<PropsWithChildren<Props>> = ({children, showNav = false}) => {
+const style = showNav ? '' : s.gridHaveOneCol
     return (
-        <div>
+        <div className={s.container + ' ' + style}>
             <Header/>
-            <main>{children}</main>
+            {showNav && (
+                <Nav/>
+            )}
+            <Main>{children}</Main>
         </div>
     )
 }
