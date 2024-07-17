8a62328d78c938fe1cbf379fce8473b430bbf981
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-17 22:49:05 +0300

    IN-5 fix: messenger - use PageWrapper and Layout

--- front-next-app/src/pages/messenger/index.tsx
+++ front-next-app/src/pages/messenger/index.tsx
@@ -1,9 +1,13 @@
-export default function Messenger() {
-  return (
-    <>
-      <main>
-          Messenger
-      </main>
-    </>
-  )
+import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
+import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+
+export function Messenger() {
+    return (
+        <PageWrapper>
+            Messenger
+        </PageWrapper>
+    )
 }
+
+Messenger.getLayout = getLayout
+export default Messenger
