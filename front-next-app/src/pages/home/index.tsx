460a8037a763b38758976514b5fbdc306bebb8ab
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-17 22:48:56 +0300

    IN-5 fix: home - use PageWrapper and Layout

--- front-next-app/src/pages/home/index.tsx
+++ front-next-app/src/pages/home/index.tsx
@@ -1,9 +1,12 @@
-export default function Home() {
+import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
+import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+
+export function Home() {
   return (
-    <>
-      <main>
+    <PageWrapper>
           Home
-      </main>
-    </>
+    </PageWrapper>
   )
 }
+Home.getLayout = getLayout
+export default Home
