a1b1558fb98748dccc8ec4953ebc28ea1a9db2a1
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-17 22:49:32 +0300

    IN-5 fix: privacy-policy - use PageWrapper and Layout

--- front-next-app/src/pages/privacy-policy/index.tsx
+++ front-next-app/src/pages/privacy-policy/index.tsx
@@ -1,9 +1,13 @@
-export default function PrivacyPolicy() {
-  return (
-    <>
-      <main>
-          PrivacyPolicy
-      </main>
-    </>
-  )
+import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
+import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+
+export function PrivacyPolicy() {
+    return (
+        <PageWrapper>
+            PrivacyPolicy
+        </PageWrapper>
+    )
 }
+
+PrivacyPolicy.getLayout = getLayout
+export default PrivacyPolicy
