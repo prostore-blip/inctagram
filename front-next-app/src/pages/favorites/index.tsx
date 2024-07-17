f3e7c55ec11feac7bc786660f1f7647b4ca5580f
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-18 00:22:59 +0300

    IN-5 fix: wrap Favorites in a NavLayout

--- front-next-app/src/pages/favorites/index.tsx
+++ front-next-app/src/pages/favorites/index.tsx
@@ -1,5 +1,5 @@
 import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
-import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+import {getNavLayout} from "../../../components/Layout/NavLayout/NavLayout";
 
 export function Favorites() {
     return (
@@ -9,5 +9,5 @@ export function Favorites() {
 
     )
 }
-Favorites.getLayout = getLayout
+Favorites.getLayout = getNavLayout
 export default Favorites;
