29ac4e678f4d470c00e024cb59eb39de5d8097a6
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-17 22:48:25 +0300

    IN-5 fix: favorites - use PageWrapper and Layout

--- front-next-app/src/pages/favorites/index.tsx
+++ front-next-app/src/pages/favorites/index.tsx
@@ -1,9 +1,13 @@
-export default function Favorites() {
-  return (
-    <>
-      <main>
-          Favorites
-      </main>
-    </>
-  )
+import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
+import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+
+export function Favorites() {
+    return (
+        <PageWrapper>
+            Favorites
+        </PageWrapper>
+
+    )
 }
+Favorites.getLayout = getLayout
+export default Favorites;
