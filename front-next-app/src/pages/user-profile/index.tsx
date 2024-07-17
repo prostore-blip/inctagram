db3debb7f162d7bbc67ca1a29a71a6a60ae7b23d
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-17 22:50:49 +0300

    IN-5 fix: user-profile - use PageWrapper and Layout

--- front-next-app/src/pages/user-profile/index.tsx
+++ front-next-app/src/pages/user-profile/index.tsx
@@ -1,9 +1,13 @@
-export default function PublicPage() {
-  return (
-    <>
-      <main>
-          PublicPage
-      </main>
-    </>
-  )
+import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
+import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+
+export function UserProfile() {
+    return (
+        <PageWrapper>
+            UserProfile
+        </PageWrapper>
+    )
 }
+
+UserProfile.getLayout = getLayout
+export default UserProfile
