692637ffd6a3e4db0609037e91d48d8efa5993bf
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-17 22:50:35 +0300

    IN-5 fix: terms-of-service - use PageWrapper and Layout

--- front-next-app/src/pages/terms-of-service/index.tsx
+++ front-next-app/src/pages/terms-of-service/index.tsx
@@ -1,9 +1,13 @@
-export default function TermsOfService() {
-  return (
-    <>
-      <main>
-          TermsOfService
-      </main>
-    </>
-  )
+import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
+import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+
+export function TermsOfService() {
+    return (
+        <PageWrapper>
+            TermsOfService
+        </PageWrapper>
+    )
 }
+
+TermsOfService.getLayout = getLayout
+export default TermsOfService
