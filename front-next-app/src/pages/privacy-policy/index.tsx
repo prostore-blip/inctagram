23252a2e876df3d55cfdef5709f3c882ab8f60a4
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-17 21:29:35 +0300

    IN-5 feat: add two new pages

---
+++ front-next-app/src/pages/privacy-policy/index.tsx
@@ -0,0 +1,9 @@
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <main>
+          PrivacyPolicy
+      </main>
+    </>
+  )
+}
---
+++ front-next-app/src/pages/terms-of-service/index.tsx
@@ -0,0 +1,9 @@
+export default function TermsOfService() {
+  return (
+    <>
+      <main>
+          TermsOfService
+      </main>
+    </>
+  )
+}
