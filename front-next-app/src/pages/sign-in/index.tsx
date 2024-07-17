2d56616be9338844e801419fe2e89284cbc28107
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-17 22:50:04 +0300

    IN-5 fix: signIn - use PageWrapper and Layout

--- front-next-app/src/pages/sign-in/index.tsx
+++ front-next-app/src/pages/sign-in/index.tsx
@@ -1,9 +1,13 @@
-export default function SignIn() {
-  return (
-    <>
-      <main>
-        SignIn
-      </main>
-    </>
-  )
+import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
+import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+
+export function SignIn() {
+    return (
+        <PageWrapper>
+            SignIn
+        </PageWrapper>
+    )
 }
+
+SignIn.getLayout = getLayout
+export default SignIn
