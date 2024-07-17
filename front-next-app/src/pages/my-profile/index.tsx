86960699710fea1606d6ed22ae865da0432c27eb
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-18 00:23:27 +0300

    IN-5 fix: wrap MyProfile in a NavLayout

--- front-next-app/src/pages/my-profile/index.tsx
+++ front-next-app/src/pages/my-profile/index.tsx
@@ -1,5 +1,5 @@
 import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
-import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+import {getNavLayout} from "../../../components/Layout/NavLayout/NavLayout";
 
 export function MyProfile() {
     return (
@@ -9,5 +9,5 @@ export function MyProfile() {
     )
 }
 
-MyProfile.getLayout = getLayout
+MyProfile.getLayout = getNavLayout
 export default MyProfile
